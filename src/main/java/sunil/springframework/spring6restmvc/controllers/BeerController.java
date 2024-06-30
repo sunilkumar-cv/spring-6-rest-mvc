08747240adb18ef977ecc832fda21ec90529d253
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-07-01 04:55:32 +0530

    Rest API to list the beers

--- src/main/java/sunil/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/sunil/springframework/spring6restmvc/controllers/BeerController.java
@@ -3,17 +3,26 @@ package sunil.springframework.spring6restmvc.controllers;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sunil.springframework.spring6restmvc.model.Beer;
 import sunil.springframework.spring6restmvc.services.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+//@Controller
+@RestController
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers() {
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
 
