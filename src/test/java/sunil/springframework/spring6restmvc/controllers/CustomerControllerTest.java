84de0746c4084c70db84b6094dd270e00a3670ce
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-07-06 08:52:01 +0530

    Assignment - Test GET Customer

---
+++ src/test/java/sunil/springframework/spring6restmvc/controllers/CustomerControllerTest.java
@@ -0,0 +1,54 @@
+package sunil.springframework.spring6restmvc.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import sunil.springframework.spring6restmvc.model.Customer;
+import sunil.springframework.spring6restmvc.services.CustomerService;
+import sunil.springframework.spring6restmvc.services.CustomerServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
+    @Test
+    void testGetCustomerById() throws Exception {
+        Customer testCustomer = customerServiceImpl.getAllCustomers().getFirst();
+
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())))
+                .andExpect(jsonPath("$.name", is(testCustomer.getName())));
+    }
+
+    @Test
+    void testgetAllCustomers() throws Exception {
+        given(customerService.getAllCustomers()).willReturn(customerServiceImpl.getAllCustomers());
+
+        mockMvc.perform(get("/api/v1/customer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.size()", is(3)));
+    }
+}
