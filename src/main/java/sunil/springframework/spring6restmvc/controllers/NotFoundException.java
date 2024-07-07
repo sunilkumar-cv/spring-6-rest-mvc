1be9bebaab7fefa6b39aa578ff3e8749471bb217
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-07-07 22:33:15 +0530

    Using @ResponseStatus annotation

--- src/main/java/sunil/springframework/spring6restmvc/controllers/NotFoundException.java
+++ src/main/java/sunil/springframework/spring6restmvc/controllers/NotFoundException.java
@@ -1,5 +1,9 @@
 package sunil.springframework.spring6restmvc.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
