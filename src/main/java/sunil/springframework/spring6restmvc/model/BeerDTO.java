fb93de76cd108ee5ef8a07cf46dcc8fa5184854c
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-07-17 05:34:20 +0530

    Controller Binding Validation

--- src/main/java/sunil/springframework/spring6restmvc/model/BeerDTO.java
+++ src/main/java/sunil/springframework/spring6restmvc/model/BeerDTO.java
@@ -1,5 +1,7 @@
 package sunil.springframework.spring6restmvc.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -12,6 +14,9 @@ import java.util.UUID;
 public class BeerDTO {
     private UUID id;
     private Integer version;
+
+    @NotNull
+    @NotBlank
     private String beerName;
     private BeerStyle beerStyle;
     private String upc;
