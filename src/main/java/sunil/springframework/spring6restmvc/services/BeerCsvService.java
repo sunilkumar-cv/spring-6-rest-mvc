8f878b9fb0bb4cfe12a2d1855d56fa69349ea41e
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-07-21 22:34:05 +0530

    CSV Parse Service

---
+++ src/main/java/sunil/springframework/spring6restmvc/services/BeerCsvService.java
@@ -0,0 +1,10 @@
+package sunil.springframework.spring6restmvc.services;
+
+import sunil.springframework.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.util.List;
+
+public interface BeerCsvService {
+    List<BeerCSVRecord> convertCSV(File csvFile);
+}
---
+++ src/main/java/sunil/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -0,0 +1,24 @@
+package sunil.springframework.spring6restmvc.services;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import sunil.springframework.spring6restmvc.model.BeerCSVRecord;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class BeerCsvServiceImpl implements BeerCsvService {
+    @Override
+    public List<BeerCSVRecord> convertCSV(File csvFile) {
+        try {
+            List<BeerCSVRecord> beerCSVRecords
+                    = new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
+                    .withType(BeerCSVRecord.class)
+                    .build().parse();
+            return beerCSVRecords;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
